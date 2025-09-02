9ba2c116f19420317084aeb43138f8dfafe80c64
alikerim1021 <alikerimank@gmail.com>
2025-09-02 14:16:52 +0300

    Add files via upload

--- main.py
+++
@@ -1,7 +0,0 @@
-from keep_alive import keep_alive
-
-# Önce web sunucusunu çalıştır (UptimeRobot bunu ping'leyecek)
-keep_alive()
-
-# Sonra botu başlat
-import bot  # bot.run() bot.py içinde çağrıldığı için burada import etmek yeterli
